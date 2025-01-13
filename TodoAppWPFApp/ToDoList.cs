45d496792a17f7e8dfb56bf8f83516938455d03f
shacro <emil.e.k7@outlook.com>
2025-01-13 16:08:47 +0100

    Class ToDoList Logik.

---
+++ TodoAppWPFApp/ToDoList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoAppWPFApp
+{
+    /// <summary>
+    /// Hanterar logiken för att lägga till, ta bort och visa todo-items.
+    /// </summary>
+    public class ToDoList
+    {
+        private List<string> tasks = new List<string>();
+        public void AddToList(string task)
+        {
+            tasks.Add(task);
+        }
+        public void RemoveTasks(int[] indexes)
+        {
+            foreach (int index in indexes)
+            {
+                if (index >= 0 && index < tasks.Count)
+                {
+                    tasks.RemoveAt(index);
+                }
+            }
+        }
+        public List<string> GetAllTasks()
+        {
+            return tasks;
+        }
+
+    }
+}
